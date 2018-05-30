accefaf954777c62287228c114e617d473e62233
Tyler - Dinh Duy Linh <linh.dinh@orientsoftware.net>
2018-05-30 14:53:44 +0700

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  categories: Array<{id: number, name: string, icon: string}>;
 
+  constructor(public navCtrl: NavController) {
+  
+    this.categories = [
+      { id: 1, name: 'Những Câu Hay Trả Lời Sai', icon: 'md-close-circle' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: 'md-star' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' },
+      { id: 2, name: 'Những Câu Đánh Dấu', icon: '' }
+    ];
+  
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(HomePage, {
+      item: item
+    });
   }
 
 }
